0d001454943c71113cf2125b102502e85b2fd2cb
AupAupAup <daniel.09635@web.de>
2023-11-27 14:52:17 +0100

    the parser works

--- XML/parsing.py
+++ XML/parsing.py
@@ -1,5 +1,10 @@
 import xml.dom.minidom
-document = xml.dom.minidom.parse('deliveries.xml')
+document = xml.dom.minidom.parse('/home/fnk/Code/Code/XML/deliveries.xml')
 article_list = document.getElementsByTagName("article")
 
-print(article_list)
+i=0
+for article in article_list:
+    supplier = document.getElementsByTagName("supplier")[i].childNodes[0].nodeValue
+    name = document.getElementsByTagName("name")[i].childNodes[0].nodeValue
+    print(f"{supplier} supplies: {name}")
+    i += 1
